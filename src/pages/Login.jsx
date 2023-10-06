16569a885e17edbf2b1a51ccec54b08b193eecd2
Mohammad Ashraf <mohd.ashraf@tothenew.com>
2023-10-06 22:09:44 +0530

    prod error: fixing error attempt 5

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -35,7 +35,7 @@ const Login = ()=> {
 
   useEffect(()=>{
     const fetchAllUsers = async() =>{
-      const res = await axios.get("https://buzzz-backend.vercel.app/users");
+      const res = await axios.get("/users");
       console.log('response--------------------',res?.data)
       setAllUsers(res?.data);
     }
